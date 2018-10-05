dbd89690e90c80d298da6d5beaec896920116aa3
unknown <arthon.maertens@student.howest.be>
2018-10-05 14:13:01 +0200

    added History function

--- src/GarbageServices/Devide2Digits.cs
+++ src/GarbageServices/Devide2Digits.cs
@@ -17,12 +17,20 @@ namespace GarbageServices
         [FunctionName("Devide2Digits")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "rekenmachine/delen/{digit1}/{digit2}")]HttpRequest req, int digit1, int digit2, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            int devider = digit1 / digit2;
- 
-            return devider != null
-                ? (ActionResult)new OkObjectResult($"De deler bedraagt: {devider}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            try
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
+                if (digit2 == 0)
+                {
+                    return new BadRequestResult();
+                }
+                decimal devider = digit1 / digit2;
+                return new OkObjectResult(devider);
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(500);
+            }
         }
     }
 }
